702cac300c56cf8d5b17c11bd8ae8de81b373ab4
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 18:41:07 +0530

    Added: Insert values on table

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,12 +1,19 @@
-#CREATE DATABASE payroll_service;
+CREATE DATABASE payroll_service;
 USE  payroll_service;
 SELECT DATABASE();
 
-CREATE TABLE employee_payroll (	
+CREATE TABLE employee_payroll(	
   id 			INT unsigned  NOT NULL AUTO_INCREMENT, 	
   name 			VARCHAR(150)  NOT NULL,		 	
   salary 		Double        NOT NULL,		 	
   start 		DATE          NOT NULL,			 	
-  PRIMARY KEY 		(id)				 	
+  PRIMARY KEY(id)				 	
 );
-DESCRIBE employee_payroll;
+
+DESCRIBE employee_payroll; 
+
+INSERT INTO employee_payroll (name,salary,start) VALUES
+( 'Sippora', 300000.00, '2018-01-03' ),
+( 'Terisa', 200000.00, '2019-11-13' ),
+( 'Surbhi', 300000.00, '2020-05-21' );
+
