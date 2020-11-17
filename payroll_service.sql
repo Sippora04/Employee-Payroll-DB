6f50928b0a5dddcdaa2075fb852b393b7900e646
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 18:47:51 +0530

    Added: retrieve all data from table

--- payroll_service.sql
+++ payroll_service.sql
@@ -2,7 +2,7 @@ CREATE DATABASE payroll_service;
 USE  payroll_service;
 SELECT DATABASE();
 
-CREATE TABLE employee_payroll(	
+CREATE TABLE employee_payroll (	
   id 			INT unsigned  NOT NULL AUTO_INCREMENT, 	
   name 			VARCHAR(150)  NOT NULL,		 	
   salary 		Double        NOT NULL,		 	
@@ -13,7 +13,8 @@ CREATE TABLE employee_payroll(
 DESCRIBE employee_payroll; 
 
 INSERT INTO employee_payroll (name,salary,start) VALUES
-( 'Sippora', 300000.00, '2018-01-03' ),
-( 'Terisa', 200000.00, '2019-11-13' ),
-( 'Surbhi', 300000.00, '2020-05-21' );
-
+( 'Samuel', 300000.00, '2017-04-04' ),
+( 'Akshay', 400000.00, '2018-10-11' ),
+( 'David', 200000.00, '2020-06-22' );
+    
+SELECT * FROM employee_payroll; 
