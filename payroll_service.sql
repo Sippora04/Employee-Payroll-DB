63b34caba954b9bfb909da1bb9eae7b1048f8972
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 19:48:56 +0530

    Added: Sum, avg, min, max, count

--- payroll_service.sql
+++ payroll_service.sql
@@ -27,3 +27,11 @@ ALTER TABLE employee_payroll ADD gender CHAR(1) AFTER name;
 UPDATE employee_payroll SET gender = 'F' where name = 'Sippora';
 UPDATE employee_payroll SET gender = 'M' WHERE name = 'Bill' or name = 'CHARLIE';
 UPDATE employee_payroll SET salary = 3000000.00 WHERE name = 'Terisa';
+
+SELECT AVG(salary) FROM employee_payroll 			
+WHERE gender = 'M' GROUP BY gender; 	
+SELECT gender, AVG(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, COUNT(name) FROM employee_payroll GROUP BY gender;
+SELECT gender, SUM(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, MIN(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, MAX(salary) FROM employee_payroll GROUP BY gender;
