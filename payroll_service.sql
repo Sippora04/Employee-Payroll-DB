20cb3574b0f2eadd2e5ed4a124723e8874810f37
Sippora04 <sippora.toppo95@gmail.com>
2020-11-19 01:08:27 +0530

    Added: create corresponding tables

--- payroll_service.sql
+++ payroll_service.sql
@@ -60,3 +60,63 @@ UPDATE employee_payroll SET department = 'Sales' WHERE name = 'Terisa';
 INSERT INTO employee_payroll(name, department, gender, basic_pay, deductions, taxable_pay, tax, net_pay, start) VALUES
 ('Terisa', 'Marketing', 'F', 3000000.00, 1000000.00, 2000000.00, 500000.00, 1500000.00, '2018-01-03');
 SELECT * FROM employee_payroll WHERE name = 'Terisa';
+
+#UC11
+CREATE TABLE employee 						
+(
+ id 		    INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ company_id 	INT  REFERENCES company(company_id),
+ employee_name  VARCHAR(50)    NOT NULL,
+ phone_number 	BIGINT         NOT NULL,
+ address 	    VARCHAR(250)   NOT NULL DEFAULT 'TBD',
+ gender 	    CHAR(1)        NOT NULL
+);
+CREATE TABLE payroll
+(
+ emp_id 	    INT REFERENCES employee(id),
+ basic_pay 	    DOUBLE NOT NULL,
+ deductions 	DOUBLE NOT NULL,
+ taxable_pay 	DOUBLE NOT NULL,
+ tax 		    DOUBLE NOT NULL,
+ net_pay 	    DOUBLE NOT NULL
+);
+CREATE TABLE company						
+(
+ company_id 	INT         PRIMARY KEY, 				
+ company_name 	VARCHAR(100) NOT NULL
+);
+CREATE TABLE department
+(
+ dept_id 	INT         PRIMARY KEY,
+ dept_name 	VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE employee_department
+(
+ emp_id 	INT REFERENCES employee(id),
+ dept_id 	INT REFERENCES department(dept_id)
+);
+
+INSERT INTO company VALUES 					
+ 	(1,'ABC'),
+ 	(2,'DEF'),
+ 	(3,'GHI');
+INSERT INTO employee VALUES 					
+ 	(101, 1, 'Bill', '9876543210', 'California', 'M' ),
+	(102, 1, 'Terisa', '8876543211', 'San Francisco', 'F' ),
+	(103, 2, 'Charlie', '7876543212', 'New York', 'M' );
+INSERT INTO payroll VALUES 			
+ 	(101,100000,10000,90000,5000,85000),
+ 	(102,200000,20000,180000,10000,170000),
+ 	(103,80000,6000,74000,2000,72000);
+INSERT INTO department VALUES  		
+ 	(11, 'Sales'),
+ 	(12, 'Marketing'),
+ 	(13, 'Testing'),
+ 	(14, 'Management');
+    INSERT INTO employee_department VALUES  				
+ 	(101,13),
+ 	(102,12),
+ 	(102,11),
+ 	(103,14);
+
